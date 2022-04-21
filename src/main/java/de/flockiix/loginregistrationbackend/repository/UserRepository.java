cc2268b3c20f16e69d17eba53e0ab38d594120f1
FlockiiX <florian@berger-bayern.de>
2022-04-21 21:20:32 +0200

    UserRepository: deleted unnecessary queries

--- src/main/java/de/flockiix/loginregistrationbackend/repository/UserRepository.java
+++ src/main/java/de/flockiix/loginregistrationbackend/repository/UserRepository.java
@@ -1,13 +1,10 @@
 package de.flockiix.loginregistrationbackend.repository;
 
-import de.flockiix.loginregistrationbackend.enumeration.Role;
 import de.flockiix.loginregistrationbackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Repository
@@ -17,19 +14,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT u FROM User u WHERE u.displayName = ?1")
     Optional<User> findUserByDisplayName(String displayName);
-
-    @Transactional
-    @Modifying
-    @Query("UPDATE User u SET u.isActive = TRUE, u.isEmailVerified = TRUE, u.role = ?2 WHERE u.email = ?1")
-    void enableUser(String email, Role role);
-
-    @Transactional
-    @Modifying
-    @Query("UPDATE User u SET u.password = ?2 WHERE u.email = ?1")
-    void updatePassword(String email, String password);
-
-    @Transactional
-    @Modifying
-    @Query("UPDATE User u SET u.isUsing2FA = ?2 WHERE u.email = ?1")
-    void update2FA(String email, boolean using2FA);
 }

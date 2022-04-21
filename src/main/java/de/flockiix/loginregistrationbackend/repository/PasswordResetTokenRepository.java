4a4682f4f582d7dd2005ece3764987e9cce70605
FlockiiX <florian@berger-bayern.de>
2022-04-21 21:22:11 +0200

    PasswordResetTokenRepository: deleted unnecessary query

--- src/main/java/de/flockiix/loginregistrationbackend/repository/PasswordResetTokenRepository.java
+++ src/main/java/de/flockiix/loginregistrationbackend/repository/PasswordResetTokenRepository.java
@@ -2,12 +2,10 @@ package de.flockiix.loginregistrationbackend.repository;
 
 import de.flockiix.loginregistrationbackend.model.PasswordResetToken;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import javax.transaction.Transactional;
-import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,8 +13,6 @@ public interface PasswordResetTokenRepository extends JpaRepository<PasswordRese
     @Query("SELECT prt FROM PasswordResetToken prt WHERE prt.token = ?1")
     Optional<PasswordResetToken> findPasswordResetTokenByToken(String token);
 
-    @Transactional
-    @Modifying
-    @Query("UPDATE PasswordResetToken prt SET prt.confirmedAt = ?2 WHERE prt.token = ?1")
-    void updateConfirmedAt(String token, LocalDateTime confirmedAt);
+    @Query(value = "SELECT prt FROM PasswordResetToken prt WHERE prt.user.email = ?1 ORDER BY prt.createdAt ASC")
+    List<PasswordResetToken> findPasswordResetTokensByUserEmail(String email);
 }

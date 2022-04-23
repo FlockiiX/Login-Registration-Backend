a16e07a12646c756e71ecf305cdca05eb304a6f5
FlockiiX <florian@berger-bayern.de>
2022-04-23 10:28:35 +0200

    PasswordResetTokenService: refactor

--- src/main/java/de/flockiix/loginregistrationbackend/service/PasswordResetTokenService.java
+++ src/main/java/de/flockiix/loginregistrationbackend/service/PasswordResetTokenService.java
@@ -1,13 +1,16 @@
 package de.flockiix.loginregistrationbackend.service;
 
 import de.flockiix.loginregistrationbackend.model.PasswordResetToken;
+import de.flockiix.loginregistrationbackend.model.User;
 
 import java.util.Optional;
 
 public interface PasswordResetTokenService {
-    void savePasswordResetToken(PasswordResetToken passwordResetToken);
+    PasswordResetToken createPasswordResetTokenForUser(User user);
 
     Optional<PasswordResetToken> getPasswordResetTokenByToken(String token);
 
     void setPasswordResetTokenConfirmedAt(String token);
+
+    boolean isAllowedToRequestPasswordResetToken(String email);
 }
--- src/main/java/de/flockiix/loginregistrationbackend/service/impl/PasswordResetTokenServiceImpl.java
+++ src/main/java/de/flockiix/loginregistrationbackend/service/impl/PasswordResetTokenServiceImpl.java
@@ -1,16 +1,17 @@
 package de.flockiix.loginregistrationbackend.service.impl;
 
+import de.flockiix.loginregistrationbackend.exception.TokenNotFoundException;
 import de.flockiix.loginregistrationbackend.model.PasswordResetToken;
+import de.flockiix.loginregistrationbackend.model.User;
 import de.flockiix.loginregistrationbackend.repository.PasswordResetTokenRepository;
 import de.flockiix.loginregistrationbackend.service.PasswordResetTokenService;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Service
-@Transactional
 public class PasswordResetTokenServiceImpl implements PasswordResetTokenService {
     private final PasswordResetTokenRepository passwordResetTokenRepository;
 
@@ -19,8 +20,12 @@ public class PasswordResetTokenServiceImpl implements PasswordResetTokenService 
     }
 
     @Override
-    public void savePasswordResetToken(PasswordResetToken passwordResetToken) {
-        passwordResetTokenRepository.save(passwordResetToken);
+    public PasswordResetToken createPasswordResetTokenForUser(User user) {
+        return passwordResetTokenRepository.save(new PasswordResetToken(
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(15),
+                user
+        ));
     }
 
     @Override
@@ -30,6 +35,18 @@ public class PasswordResetTokenServiceImpl implements PasswordResetTokenService 
 
     @Override
     public void setPasswordResetTokenConfirmedAt(String token) {
-        passwordResetTokenRepository.updateConfirmedAt(token, LocalDateTime.now());
+        PasswordResetToken passwordResetToken = passwordResetTokenRepository
+                .findPasswordResetTokenByToken(token)
+                .orElseThrow(() -> new TokenNotFoundException("Token not found"));
+        passwordResetToken.setConfirmedAt(LocalDateTime.now());
+    }
+
+    @Override
+    public boolean isAllowedToRequestPasswordResetToken(String email) {
+        List<PasswordResetToken> resetTokenList = passwordResetTokenRepository.findPasswordResetTokensByUserEmail(email);
+        if (resetTokenList.isEmpty())
+            return true;
+
+        return resetTokenList.get(0).getCreatedAt().isBefore(LocalDateTime.now().minusDays(7));
     }
 }

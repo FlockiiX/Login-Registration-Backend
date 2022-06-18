b3015c8712e658a5d87e002dbe21e724b62d9a11
FlockiiX <florian@berger-bayern.de>
2022-06-18 11:17:53 +0200

    JwtConstant: delete unused constant

--- src/main/java/de/flockiix/loginregistrationbackend/constant/JwtConstant.java
+++ src/main/java/de/flockiix/loginregistrationbackend/constant/JwtConstant.java
@@ -1,7 +1,6 @@
 package de.flockiix.loginregistrationbackend.constant;
 
 public class JwtConstant {
-    public static final String JWT_TOKEN_HEADER = "Jwt-Access-Token";
     public static final String ISSUER = "FlockiiX";
     public static final String AUDIENCE = "FlockiiX User Management";
     public static final String[] PUBLIC_URLS = {"/api/**/auth/login", "/api/**/auth/refresh", "/api/**/user/register", "/api/**/user/confirm", "/api/**/user/resetPassword"};

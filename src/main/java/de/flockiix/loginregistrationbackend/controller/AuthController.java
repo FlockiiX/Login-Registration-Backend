46a2b54b0536c8c83c4013f7e5fc9112ed6fbb6f
FlockiiX <florian@berger-bayern.de>
2022-06-18 11:14:46 +0200

    AuthController: move tokens in body

--- src/main/java/de/flockiix/loginregistrationbackend/controller/AuthController.java
+++ src/main/java/de/flockiix/loginregistrationbackend/controller/AuthController.java
@@ -1,19 +1,15 @@
 package de.flockiix.loginregistrationbackend.controller;
 
 import de.flockiix.loginregistrationbackend.advice.ControllerAdvice;
-import de.flockiix.loginregistrationbackend.config.properties.JwtProperties;
-import de.flockiix.loginregistrationbackend.constant.JwtConstant;
 import de.flockiix.loginregistrationbackend.exception.InvalidRefreshTokenException;
 import de.flockiix.loginregistrationbackend.jwt.JwtTokenProvider;
+import de.flockiix.loginregistrationbackend.model.JwtAuthenticationResponse;
 import de.flockiix.loginregistrationbackend.model.User;
+import de.flockiix.loginregistrationbackend.payload.RefreshTokenRequest;
 import de.flockiix.loginregistrationbackend.service.UserService;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.concurrent.TimeUnit;
 
 import static org.springframework.http.HttpStatus.OK;
 
@@ -22,19 +18,17 @@ import static org.springframework.http.HttpStatus.OK;
 public class AuthController extends ControllerAdvice {
     private final UserService userService;
     private final JwtTokenProvider jwtTokenProvider;
-    private final JwtProperties jwtProperties;
 
-    public AuthController(UserService userService, JwtTokenProvider jwtTokenProvider, JwtProperties jwtProperties) {
+    public AuthController(UserService userService, JwtTokenProvider jwtTokenProvider) {
         this.userService = userService;
         this.jwtTokenProvider = jwtTokenProvider;
-        this.jwtProperties = jwtProperties;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<User> login(@RequestBody User user) {
+    public ResponseEntity<?> login(@RequestBody User user) {
         User loginUser = userService.login(user.getEmail(), user.getPassword());
-        HttpHeaders jwtHeader = getJwtHeader(loginUser, jwtTokenProvider.generateJwtRefreshToken(loginUser));
-        return new ResponseEntity<>(loginUser, jwtHeader, HttpStatus.OK);
+        JwtAuthenticationResponse authenticationResponse = getJwtAuthenticationResponse(loginUser, jwtTokenProvider.generateJwtRefreshToken(loginUser));
+        return new ResponseEntity<>(authenticationResponse, HttpStatus.OK);
     }
 
     @GetMapping("/logout")
@@ -44,10 +38,11 @@ public class AuthController extends ControllerAdvice {
     }
 
     @GetMapping("/refresh")
-    public ResponseEntity<?> refreshToken(@CookieValue(name = "refresh-token", defaultValue = "") String refreshToken) {
-        if (refreshToken.isBlank())
+    public ResponseEntity<?> refreshToken(@RequestBody RefreshTokenRequest refreshTokenRequest) {
+        if (refreshTokenRequest == null || refreshTokenRequest.refreshToken().isBlank())
             throw new InvalidRefreshTokenException("Invalid refresh token");
 
+        String refreshToken = refreshTokenRequest.refreshToken();
         String email = jwtTokenProvider.getSubject(refreshToken, false);
         User user = userService
                 .findUserByEmail(email)
@@ -56,23 +51,12 @@ public class AuthController extends ControllerAdvice {
         if (!jwtTokenProvider.isTokenValid(email, refreshToken, false) || jwtTokenProvider.getRefreshTokenCountFromToken(refreshToken) != user.getRefreshTokenCount())
             throw new InvalidRefreshTokenException("Invalid refresh token");
 
-        HttpHeaders jwtHeader = getJwtHeader(user, refreshToken);
-        return new ResponseEntity<>("Token refreshed", jwtHeader, HttpStatus.OK);
+        JwtAuthenticationResponse authenticationResponse = getJwtAuthenticationResponse(user, refreshToken);
+        return new ResponseEntity<>(authenticationResponse, HttpStatus.OK);
     }
 
-    private HttpHeaders getJwtHeader(User user, String refreshToken) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(JwtConstant.JWT_TOKEN_HEADER, jwtTokenProvider.generateJwtAccessToken(user));
-        headers.add(HttpHeaders.SET_COOKIE, getJwtRefreshTokenCookie(refreshToken).toString());
-        return headers;
-    }
-
-    private ResponseCookie getJwtRefreshTokenCookie(String refreshToken) {
-        return ResponseCookie.from("refresh-token", refreshToken)
-                .httpOnly(true)
-                .secure(true)
-                .sameSite("strict")
-                .maxAge(TimeUnit.DAYS.toSeconds(jwtProperties.getRefreshTokenExpiresIn()))
-                .build();
+    private JwtAuthenticationResponse getJwtAuthenticationResponse(User user, String refreshToken) {
+        String accessToken = jwtTokenProvider.generateJwtAccessToken(user);
+        return new JwtAuthenticationResponse(accessToken, refreshToken);
     }
 }

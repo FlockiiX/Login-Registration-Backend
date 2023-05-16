ae259942587573594716e676cd6942281fa2bd33
othmane099 <48645187+othmane099@users.noreply.github.com>
2023-05-16 12:21:18 +0100

    Update PasswordDto.java
    change it from class to record

--- src/main/java/de/flockiix/loginregistrationbackend/dto/PasswordDto.java
+++ src/main/java/de/flockiix/loginregistrationbackend/dto/PasswordDto.java
@@ -4,22 +4,15 @@ import de.flockiix.loginregistrationbackend.validation.annotation.ValidPassword;
 
 import javax.validation.constraints.NotBlank;
 
-public class PasswordDto {
-    private final String oldPassword;
-    @ValidPassword
-    @NotBlank(message = "Password is mandatory")
-    private final String newPassword;
-
+public record PasswordDto(String oldPassword,
+                          @ValidPassword @NotBlank(message = "Password is mandatory") String newPassword) {
     public PasswordDto(String oldPassword, String newPassword) {
         this.oldPassword = oldPassword;
         this.newPassword = newPassword;
     }
 
-    public String getOldPassword() {
-        return oldPassword;
-    }
-
-    public String getNewPassword() {
+    @Override
+    public String newPassword() {
         return newPassword;
     }
 }
